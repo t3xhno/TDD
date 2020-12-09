f8832af873d5bb2cfa42537a62631eb184364d81
texhnolainx <t3xhnolainx@gmail.com>
2020-12-09 19:36:48 +0100

    Styling added

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -1,3 +1,5 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import Vuex from 'vuex';
 import {createLocalVue, mount} from '@vue/test-utils';
 import RestaurantList from '@/components/RestaurantList';
@@ -6,6 +8,7 @@ const findByTestId = (wrapper, testId, index) =>
   wrapper.findAll(`[data-testid="${testId}"]`).at(index);
 
 describe('RestaurantList', () => {
+  Vue.use(Vuetify);
   const records = [
     {id: 1, name: 'Sushi Place'},
     {id: 2, name: 'Pizza Place'},
